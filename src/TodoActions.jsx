8d24e2e2132fbd335c57c54b0799ec025de95146
kris_wale <40746582+kristiankf@users.noreply.github.com>
2023-08-17 09:18:01 +0000

    little refactors

--- src/TodoActions.jsx
+++ src/TodoActions.jsx
@@ -7,6 +7,7 @@ function TodoActions({ handleAddClick, handleChangeList, active, items }) {
             <span
               className="icon-contain"
               style={active == "completed" ? { display: "none" } : null}
+              title="Add task"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
